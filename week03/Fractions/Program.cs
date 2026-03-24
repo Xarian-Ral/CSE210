dc87a47096b09c734dddd7c0c5aa081300d9ee22
xarian-ral <Toaxarian@gmail.com>
2026-03-24 10:20:36 -0700

    Add files via upload

---
+++ week03/Fractions/Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        fraction f1 = new fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.getDecimalValue());
+
+        fraction f2 = new fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        fraction f3 = new fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        fraction f4 = new fraction (1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+    }
+}
